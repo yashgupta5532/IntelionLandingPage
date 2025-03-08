8830c4a7e043aff4ba4be71b7da997df6c83c88d
Yash Gupta <yashgupta5532@gmail.com>
2025-03-08 13:22:08 +0530

    pushing the files to the github

--- server/index.ts
+++ server/index.ts
@@ -58,7 +58,7 @@ app.use((req, res, next) => {
 
   // ALWAYS serve the app on port 5000
   // this serves both the API and the client
-  const port = 5000;
+  const port = 10000;
   server.listen({
     port,
     host: "0.0.0.0",
